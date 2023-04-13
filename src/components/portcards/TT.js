950633653e54180c4ed8cb477b9af59b6aa5b0ae
Shannya <niveyro101@gmail.com>
2023-04-13 02:37:30 -0400

    succesful first portfolio card

---
+++ src/components/portcards/TT.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import TTlogo from '/Users/babygirl/bootcamp/Challenges/react-portfolio/src/assets/images/TT.png'
+
+export default function TT (){
+    return (
+        <div className='card col '>
+        <img src={TTlogo} className="card-img-top" alt="Travel Tracker."/>
+        <div className='card-body'>
+          <h3 className='card-title'>Travel Tracker</h3>
+          <p className="card-text">Created using JavaScript, Express, Node, and APIs. Lets users drop, drag, and remove pins on map locations that can be searched to find. Airport selections based on pin locations. </p>
+          <div className='d-flex linkbuttons container columns '> 
+            <button type='button' href="https://bobbycharms.github.io/travel-tracker/" className="btn btn-primary column ">Deployed App
+            </button>
+            &emsp;
+            <button type='button' href="https://github.com/BobbyCharms/travel-tracker" className="btn column btn-primary">Repository
+            </button>
+          </div>
+        </div>
+    </div>
+    )
+}
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import ttLogo from '../assets/images/TT.png'
+import TT from '../components/portcards/TT'
+// import ttLogo from '../assets/images/TT.png'
 
 export default function Portfolio(){
     return(
-      <div className='card portfolio' >
+      <div className='card portfolio d-flex justify-content-even ' >
       <h2>Portfolio</h2>
-      <div className='container columns'>
-          <div className=' row-cols-2'>
-              <div className='col-sm column card'>
-                  <img src={ttLogo} className="card-img-top" alt="Travel Tracker."/>
-                  <div className='card-body'>
-                    <h3 className='card-title'>Travel Tracker</h3>
-                    <p class="card-text">Created using JavaScript, Express, Node, and APIs. Lets users drop, drag, and remove pins on map locations that can be searched to find. Upon the selection of a pin, the closest airport names and locations appear. </p>
-                    <a href="https://bobbycharms.github.io/travel-tracker/" class="btn btn-primary">Deployed App</a>
-                    {/* href='https://bobbycharms.github.io/travel-tracker/' */}
-                  </div>
-              </div>
-              <div className='col-md column card'>
-                  
-              </div>
-          </div>
-      </div>
+      <section className='container'>
+        <div className='row'>
+          <TT />
+          <TT/>
+          <TT/>
+        </div>
+      </section>
   </div>
     )
 };
