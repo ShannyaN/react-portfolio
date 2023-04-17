65346b4f980f3448de070b756102c50add233b1d
Shannya <niveyro101@gmail.com>
2023-04-17 19:23:05 -0400

    project function

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import TT from '../components/portcards/TT'
 import LifeLog from '../components/portcards/LifeLog'
+import LifeLogLogo from '../assets/images/portIcons/lifelog'
 import MySQLDB from '../components/portcards/MySQLDB'
+import MySQLDBlogo from '../assets/images/portIcons/db.png'
 import PassGen from '../components/portcards/PassGen'
+import PassGenlogo from '../assets/images/portIcons/pwd.png'
 import ReadGen from '../components/portcards/ReadGen';
+import ReadGenlogo from '../assets/images/portIcons/readme.png'
 import Weather from '../components/portcards/Weather';
+import Weatherlogo from '../assets/images/portIcons/Weather.png'
 
 export default function Portfolio(){
     return(
